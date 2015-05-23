bedf4e33d123ebc50fc378cf1cd7c0a346a13f57
UniSabina <sabina.przioda@tum.de>
2015-05-23 13:48:01 +0200

    neue  ODE von Johanes mit in den Matlab Code Ordner

--- Matlab Code/classOptimParam.m
+++ Matlab Code/classOptimParam.m
@@ -4,8 +4,8 @@ classdef classOptimParam < handle
     properties
         n = 5;         % number of intervals
         tf = 120;       % time to run from 0 to tf
-        c1 = 2;         % optimizing consumption
-        c2 = 1;       % optimizing street
+        c1 = 1;         % optimizing consumption
+        c2 = 1000;       % optimizing street
         v0 = 1;
     end
 end
