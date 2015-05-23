39020232b3a65401340cf0bb2b80554d0c58a5dc
Johanes Milz <johannes.milz@tum.de>
2015-05-23 17:57:27 +0200

    Mein erster Versuch für eine Graphik

--- Robustes Automodell/ODETest.m
+++ Robustes Automodell/ODETest.m
@@ -4,18 +4,18 @@
 
 %% Weather w
 %
-w = 0.1; 
+w = 1; 
 
 %% control
 %
 u = zeros(3,1);
-u(1) = 140;
+u(1) = 400;
 u(2) = 0;
-u(3) = 0;
+u(3) = 0.1;
 
 %% 
 %
-r =  100; % radius of test course
+r =  2000; % radius of test course
 
 %%
 %
@@ -23,7 +23,7 @@ r =  100; % radius of test course
 z0 = zeros(5,1);
 z0(3) = 10;
 
-[T, Y] = ode45( @(t, x) ODE(t, x, u, w, r), [0 120], z0);
+[T, Y] = ode15s( @(t, x) ODE(t, x, u, w, r), [0 120], z0);
 
 %%
 %
@@ -34,6 +34,8 @@ plot( Y(:, 1), Y(:, 2));
 title('y vs z')
 xlabel('y [m]')
 ylabel(' z[m]')
+
+pathlength = sum(sqrt(Y(:, 1).^2 + Y(:, 2).^2)); 
 
 subplot(2,2,2)
 
@@ -48,4 +50,6 @@ subplot(2,2,3)
 plot(T, Y(:, 3))
 xlabel('Time t [s]')
 ylabel('v(t) [m/s]')
+hold on
+plot(10, 100/3.6, 'o' )
 
