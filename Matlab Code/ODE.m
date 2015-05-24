750b2a5afeb41893a295c244b33c3ba845cb2ced
Johanes Milz <johannes.milz@tum.de>
2015-05-24 19:35:08 +0200

    Update "neue ODE von Johannes im Matlab CodeOrdner"

--- Matlab Code/ODE.m
+++ Matlab Code/ODE.m
@@ -8,7 +8,7 @@ cCCP = classCarConstantParam();     % constant Car Parameters
 A = 1.4378946874;
 %A = 0;
 mu = [0.1, 1.6, -0.8];
-fR = [0.8e-2; 0.15e-2; 0.08e-2];
+fR = [0.015; 0.005; 0.001];
 %fR = zeros(3,1);
 
 dx = zeros(5,1);
@@ -20,17 +20,17 @@ dx(2) = x(3) * sin(x(5)) + x(4) * cos(x(5));
 
 dx(3) = 1/cCCP.m *( u(1)/cCCP.R - u(2) ...
     - 0.5000 * cCCP.c_w * cCCP.rho * A * x(3)^2  ...
-    -(fR(1) + fR(2)./100./3.6 *x(3) + fR(3) * (x(3)./100./3.6)^4) * cCCP.m * cCCP.g);
+    -(fR(1) + fR(2)./30*x(3) + fR(3) * (x(3)./30)^4) * cCCP.m * cCCP.g);
 
 
 Fres_Fs = sqrt( x(3)^4 / (r)^2 + dx(3)^2 ) ...
     - (mu(1) + mu(2) * w + mu(3) * w^2) * cCCP.g;
 
 if(Fres_Fs > 0)
-    if abs(dx(3)) < 1e-3
-        error('verdammt')
-    end
-    dx(4) = x(3)^2/(r)/dx(3) * Fres_Fs;
+    %dx(4) = x(3)^2/(r)/dx(3) * Fres_Fs;
+    %dx(4) = Fres_Fs * sin(atan(x(3)^2/dx(3)/r));
+    dx(4) =  Fres_Fs/5 *(x(3)^2/(r) - 0.5 * (mu(1) + mu(2) * w + mu(3) * w^2) * cCCP.g) ;
+    %dx(4) = x(3)^2/(r)/dx(3)*Fres_Fs-0.5 *(fR(1)  + fR(2)./30*x(4) + fR(3) * (x(4)./30)^4) * cCCP.g;
 else
     dx(4) = 0;
 end
---
+++ Matlab Code/ODETest.m
@@ -0,0 +1,55 @@
+%% ODE Test
+%
+
+
+%% Weather w
+%
+w = 1; 
+
+%% control
+%
+u = zeros(3,1);
+u(1) = 400;
+u(2) = 0;
+u(3) = 0.5;
+
+%% 
+%
+r =  200; % radius of test course
+
+%%
+%
+
+z0 = zeros(5,1);
+z0(3) = 10;
+
+[T, Y] = ode15s( @(t, x) ODE(t, x, u, w, r), [0 120], z0);
+
+%%
+%
+clf
+
+subplot(2,2,1)
+plot( Y(:, 1), Y(:, 2));
+title('y vs z')
+xlabel('y [m]')
+ylabel(' z[m]')
+
+pathlength = sum(sqrt(Y(:, 1).^2 + Y(:, 2).^2)); 
+
+subplot(2,2,2)
+
+plot(T, Y(:,4));
+title('v_r(t)')
+xlabel('Time t  [s]')
+ylabel(' v_r(t) [m/s]')
+
+
+subplot(2,2,3)
+
+plot(T, Y(:, 3))
+xlabel('Time t [s]')
+ylabel('v(t) [m/s]')
+hold on
+plot(10, 100/3.6, 'o' )
+
